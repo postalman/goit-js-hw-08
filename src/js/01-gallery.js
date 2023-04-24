b8d25bf6c4c7e9f4ddfb5a6f9ebba1af6255ef8f
ChillyRide <sebastianmars78@gmail.com>
2023-04-24 20:29:16 +0300

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,6 +1,9 @@
 import "simplelightbox/dist/simple-lightbox.min.css";
+import SimpleLightbox from "simplelightbox";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+
+const gallery = document.querySelector(".gallery");
 
 console.log(galleryItems);
