f7a4312297109c67b74f6adc7339c26d9b729ab2
ChillyRide <sebastianmars78@gmail.com>
2023-04-24 23:26:50 +0300

    2 done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,21 +1,19 @@
-import Vimeo from "@vimeo/player"
+import Vimeo from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
-    const player = new Vimeo(iframe);
+const player = new Vimeo(iframe);
 
-    player.on('play', function() {
-        console.log('played the video!');
-    });
+player.on(
+  'timeupdate',
+  throttle(function (data) {
+    const currentTime = data.seconds;
+    localStorage.setItem('videoplayer-current-time', currentTime);
+  }, 1000)
+);
 
-    player.getVideoTitle().then(function(title) {
-        console.log('title:', title);
-    });
+const savedTime = localStorage.getItem('videoplayer-current-time');
 
-    player.on('timeupdate', function(data) {
-        const currentTime = data.seconds;
-       localStorage.setItem('videoplayer-current-time', currentTime);
-      });
-
-      player.setCurrentTime(currentTime).then(function(seconds) {
-        // seconds = the actual time that the player seeked to
-    })
+player.setCurrentTime(savedTime).then(function (seconds) {
+  player.play();
+});
