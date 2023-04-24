762ebd990bf586f9c1d795ef3a7f6a366b142b21
ChillyRide <sebastianmars78@gmail.com>
2023-04-24 21:45:42 +0300

    upd

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import vimeo from "@vimeo/player"
 
+const iframe = document.querySelector('iframe');
+    const player = new Vimeo.Player(iframe);
+
+    player.on('play', function() {
+        console.log('played the video!');
+    });
+
+    player.getVideoTitle().then(function(title) {
+        console.log('title:', title);
+    });
+
+    const onPlay = function(data) {
+        
+        // data is an object containing properties specific to that event
+    };
+    
+    player.on('play', onPlay);
