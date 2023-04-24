916052709e4af1a9d7b98351d7e82ac03b3947a9
ChillyRide <sebastianmars78@gmail.com>
2023-04-24 20:49:23 +0300

    upd

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -5,5 +5,13 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 const gallery = document.querySelector(".gallery");
+const galleryArray = Array.from(gallery);
+const currentInstance = new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionDelay: "250",
+  });
+
+
+
 
 console.log(galleryItems);
