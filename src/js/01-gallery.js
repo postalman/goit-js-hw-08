3900dc0918c276cda63fa4dc365fe7a76db30371
ChillyRide <sebastianmars78@gmail.com>
2023-04-27 02:06:41 +0300

    start

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,34 +1,5 @@
-import "simplelightbox/dist/simple-lightbox.min.css";
-import SimpleLightbox from "simplelightbox";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-
-const gallery = document.querySelector(".gallery");
-
-gallery.insertAdjacentHTML("beforeend", onGallery(galleryItems));
-
-
-let currentInstance = new SimpleLightbox(".gallery a", {
-  captionsData: "alt",
-  captionDelay: "250",
-});
-
-function onGallery(item) {
-  return item
-    .map(({ preview, original, description }) => {
-      return `
-    <li class="gallery__item">
-    <a class="gallery__link" href="${original}">
-      <img class="gallery__image" src="${preview}" alt="${description}" />
-    </a>
-    </li>`;
-    })
-    .join("");
-}
-
-
-
-
 
 console.log(galleryItems);
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,19 +1,1 @@
-import Vimeo from '@vimeo/player';
-import throttle from 'lodash.throttle';
-
-const iframe = document.querySelector('iframe');
-const player = new Vimeo(iframe);
-
-player.on('timeupdate', throttle(function (data) {
-    const currentTime = data.seconds;
-    localStorage.setItem('videoplayer-current-time', currentTime);
-  }, 1000)
-);
-
-const savedTime = +localStorage.getItem('videoplayer-current-time');
-
-player.setCurrentTime(savedTime);
-
-// console.log(savedTime);
-
 
