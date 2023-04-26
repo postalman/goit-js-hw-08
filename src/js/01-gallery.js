0fc98fcb3df5798cbc1ecfb925aedb1cbd515453
ChillyRide <sebastianmars78@gmail.com>
2023-04-27 02:08:15 +0300

    upd

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
+import "simplelightbox/dist/simple-lightbox.min.css";
+import SimpleLightbox from "simplelightbox";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+gallery.insertAdjacentHTML("beforeend", onGallery(galleryItems));
+
+
+let currentInstance = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: "250",
+});
+
+function onGallery(item) {
+  return item
+    .map(({ preview, original, description }) => {
+      return `
+    <li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>
+    </li>`;
+    })
+    .join("");
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,34 @@
+import throttle from "lodash.throttle"
 
+const feedbackForm = document.querySelector('.feedback-form');
+const emailInput = document.querySelector('.feedback-form input[name="email"]');
+const messageInput = document.querySelector('.feedback-form textarea[name="message"]');
+
+const STORAGE_KEY = 'feedback-form-state';
+
+feedbackForm.addEventListener('submit', onSubmit);
+
+  let formData = {};
+
+  feedbackForm.addEventListener('input', throttle(e => { 
+    formData[e.target.name] = e.target.value;
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+  }, 500));
+
+function onSubmit (e) {
+    e.preventDefault();
+    console.log(formData);
+    e.target.reset();
+    localStorage.removeItem(STORAGE_KEY);
+}
+
+const storedData = localStorage.getItem(STORAGE_KEY);
+
+if (storedData) {
+    formData = JSON.parse(storedData);
+    emailInput.value = formData.email;
+    messageInput.value = formData.message;
+}   else {
+    emailInput.value = '';
+    messageInput.value = '';
+}
